1afd56d45ea55382052647f47abb108bf38b9da1
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2023-06-10 19:31:18 +0200

    feat: add const file

---
+++ const/const.ts
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    link: 'https://github.com/team-prstmw/StarWarsQuiz',
+    img: '/images/swpixel.webp',
+    alt: 'Photo Star Wars Pixel Art',
+  },
+  {
+    link: 'https://github.com/ike-tom/space-tourism',
+    img: '/images/astronautpixel.webp',
+    alt: 'Photo of Space Tourism',
+  },
+  {
+    link: 'https://github.com/topics/oj-mat-api',
+    img: '/images/bicyclepixel.webp',
+    alt: 'Photo of the bicycle',
+  },
+  {
+    link: 'https://github.com/ike-tom/fake-news-network-FE',
+    img: '/images/fakenewsnetwork.webp',
+    alt: 'Photo of the Trollface',
+  },
+]
+
+export const email = 'mailto:tomasz.ikert.frontend@gmail.com'
+export const githubAcc = 'https://github.com/ike-tom'
+export const linkedinAcc = 'https://www.linkedin.com/in/tomaszikert/'
